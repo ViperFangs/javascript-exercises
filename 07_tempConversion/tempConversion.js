a0e1fe3b469d4b13ec051d94da56cba61b40c54f
Aarya <amathrej@asu.edu>
2022-05-14 05:00:52 -0700

    Final solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const ftoc = function(tempC) {
-let tempF = ((9/5)*tempC)+32;
-return Math.round(tempF * 10) / 10;
+const ftoc = function(tempF) {
+let tempC = (5/9)*(tempF-32);
+
+return Math.round(tempC * 10) / 10;
 };
 
-const ctof = function(tempF) {
-let tempC = (5/9)*(tempF-32);
-return Math.round(tempC * 10) / 10;
+const ctof = function(tempC) {
+let tempF = ((9/5)*tempC)+32;
+
+return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
