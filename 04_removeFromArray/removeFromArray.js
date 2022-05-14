f88f1076a3b6ea70e7f4af78bc270b654aea17f2
Aarya <amathrej@asu.edu>
2022-05-14 03:44:22 -0700

    Initial solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray, num) {
+    let outputArray=[];
+    for(const ch of userArray){
+        if(ch === num){
+            continue;
+        }
+        outputArray.push(ch);
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
