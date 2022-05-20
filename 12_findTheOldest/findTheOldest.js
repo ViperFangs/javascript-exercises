80a25dc979e022a2ddbbad1e660f349b60131f0e
Aarya <amathrej@asu.edu>
2022-05-19 23:30:40 -0700

    Write pseudo code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+/*---------------
+    Pseudo Code:
+Function takes an array,.
+Take the array.yearOfDeath and subtract with the array.yearOfBirth.
+However, if there is no yearOfDeath then set the date as the current time 
+Use ((new Date).getFullYear());
+---------------*/ 
 };
 
 // Do not edit below this line
