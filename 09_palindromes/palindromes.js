1c79394a43310038c26244b7f77ec533afa56265
Aarya <amathrej@asu.edu>
2022-05-19 19:37:44 -0700

    Finish solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    aLower = a.toLowerCase();
+    const result = aLower.replace(regex, '');
+    const output = result.split(" ").join("")
+    return output == output.split("").reverse().join("")
 };
 
 // Do not edit below this line
