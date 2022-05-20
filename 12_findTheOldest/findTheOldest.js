f997fdf73440c46b7070663271febe8f3f871652
Aarya <amathrej@asu.edu>
2022-05-19 23:36:28 -0700

    Write solution from pseudo code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -6,6 +6,19 @@ Take the array.yearOfDeath and subtract with the array.yearOfBirth.
 However, if there is no yearOfDeath then set the date as the current time 
 Use ((new Date).getFullYear());
 ---------------*/ 
+
+if(!arrayOfPeople.yearOfDeath){
+    arrayOfPeople.yearOfDeath = ((new Date).getFullYear());
+}
+
+const oldest = arrayOfPeople.sort((previousPerson, nextPerson) =>{
+    let previousPersonDOB = previousPerson.yearOfDeath - previousPerson.yearOfBirth;
+    let nextPersonDOB = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+
+    if(previousPersonDOB > nextPersonDOB) return -1
+    return 1;
+})
+
 };
 
 // Do not edit below this line
