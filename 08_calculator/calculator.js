2bf6b51b264c97c6016c418473feac8c12d8acfd
Aarya <amathrej@asu.edu>
2022-05-19 19:18:04 -0700

    Write solutions for all problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, currentNumber) => {
+    return total += currentNumber;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total, currentNumber) => {
+    return total *= currentNumber;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+  if(b <= 0){
+    return 1;
+  }
+  else {
+    return a*power(a, (b-1));
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a <= 0){
+    return 1;
+  }
+  else {
+    return a*factorial((a-1));
+  }
 };
 
 // Do not edit below this line
