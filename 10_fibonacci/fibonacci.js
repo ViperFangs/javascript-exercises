af0a2a17b2aa3917c29e67b72ff111294a33d018
Aarya <amathrej@asu.edu>
2022-05-19 23:13:08 -0700

    Write solution for fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+if(a <= 0) return 0;
+else if(a == 1) return 1;
+return fibonacci(a-1)+fibonacci(a-2);
 };
 
 // Do not edit below this line
